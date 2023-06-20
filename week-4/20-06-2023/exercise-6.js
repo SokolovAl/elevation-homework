d3b93f828d993ea56b770defb0881c7fffb96cd3
Aleksandr Sokolov <sokolovmono@gmail.com>
2023-06-20 19:40:44 +0300

    add solution for homework for 20.06.2023

--- week-4/20-06-2023/exercise-6.js
+++ week-4/20-06-2023/exercise-6.js
@@ -1,20 +1,11 @@
-// const determineWeather = temp => {
-//     if (temp > 25) {
-//         return "hot";
-//     }
-//     return "cold";
-// };
-//
-// const commentOnWeather = temp => "It's " + determineWeather(temp);
-//
-// commentOnWeather(30); //returns "It's hot"
-// commentOnWeather(22); //returns "It's cold"
+const determineWeather = temp => {
+    if (temp > 25) {
+        return "hot";
+    }
+    return "cold";
+};
 
-const run = (name) => {
-    console.log("Run, " + name + ", run!");
-};
-const yell = function (action) {
-    let env = "Forest";
-    action(env);
-};
-run(yell);
+const commentOnWeather = temp => "It's " + determineWeather(temp);
+
+commentOnWeather(30); //returns "It's hot"
+commentOnWeather(22); //returns "It's cold"
