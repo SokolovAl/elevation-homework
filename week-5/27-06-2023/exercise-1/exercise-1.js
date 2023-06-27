e9552bfb8794d312964a83a31c2c2f2f4d4419bc
Aleksandr Sokolov <sokolovmono@gmail.com>
2023-06-27 21:09:15 +0300

    add solution for exercise-1 for homework for 27.06.2023

---
+++ week-5/27-06-2023/exercise-1/exercise-1.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('#add').click(function () {
+        $('#names').append('<li>' + $('#name-input').val() + '</li>');
+        $('#name-input').val('');
+    });
+});
