3aee7f54def97668189c19464be47013f567b2fd
Aleksandr Sokolov <sokolovmono@gmail.com>
2023-06-28 15:38:40 +0300

    add solution for exercise-5 for homework for 27.06.2023

---
+++ week-5/27-06-2023/exercise-5/exercise-5.js
@@ -0,0 +1,22 @@
+const fruits = [
+    {name: "Orange", color: "orange"},
+    {name: "Banana", color: "yellow"},
+    {name: "Coconut", color: "brown"},
+    {name: "Kiwi", color: "brown"},
+    {name: "Lemon", color: "yellow"},
+    {name: "Cucumber", color: "green"},
+    {name: "Persimmon", color: "orange"},
+    {name: "Pumpkin", color: "orange"}
+]
+$(document).ready(function () {
+    $("#basket").append(
+        $.each(fruits, function (index, fruit) {
+            $("<div>")
+                .text(fruit.name)
+                .addClass(fruit.color)
+                .appendTo("#basket");
+        })
+    );
+});
+
+
