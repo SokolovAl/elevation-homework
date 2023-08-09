fa576b1b277ee476d7232d6af647f38cabfae3c8
Aleksandr Sokolov <sokolovmono@gmail.com>
2023-08-09 16:15:57 +0300

    add solution for exercise 1 for homework for 08.08.2023

---
+++ week-11/08-08-2023/react-input/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Exercise1 from "./components/Exercise1";
+
+function App() {
+    return (
+        <div className = "App">
+            <Exercise1/>
+        </div>
+    );
+}
+
+export default App;
---
+++ week-11/08-08-2023/react-input/src/components/Exercise1.js
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+
+const Exercise1 = () => {
+    const [person, setPerson] = useState({name: "", age: ""});
+
+    const handleChange = (event, property) => {
+        setPerson({...person, [property]: event.target.value});
+    };
+
+    const handleButtonClick = () => {
+        if (person.name && person.age) {
+            alert(`Come in ${person.name}, you’re ${person.age} - that’s good enough`);
+        } else {
+            alert("Please enter both name and age");
+        }
+    };
+
+    return (
+        <div>
+            <h1>Exercise 1</h1>
+            <input id = "name-input"
+                   onChange = {(e) => handleChange(e, "name")}
+                   value = {person.name}
+                   placeholder = "Enter your name"
+            />
+            <input id = "age-input"
+                   onChange = {(e) => handleChange(e, "age")}
+                   value = {person.age}
+                   placeholder = "Enter your age"
+            />
+            <button onClick = {handleButtonClick}>Go to Bar</button>
+        </div>
+    );
+};
+
+export default Exercise1;
